4fedd20dfe545a1f16dbe591d44ca6df61d2de53
grudelsud <thomasalisi@gmail.com>
2012-06-22 11:38:29 +0100

    mv h4e in separate repo

--- web/application/config/routes.php
+++ web/application/config/routes.php
@@ -38,7 +38,7 @@
 |
 */
 
-$route['default_controller'] = 'h4e';
+$route['default_controller'] = 'main';
 $route['404_override'] = '';
 
 
--- web/application/views/main/home.php
+++ web/application/views/main/home.php
@@ -2,9 +2,9 @@
 	<div class="row">
 		<div class="span12">
 			<ul class="nav nav-tabs">
-				<li class="active"><a href="#tab_feed" data-toggle="tab">Feed</a></li>
-				<li><a href="#tab_map" data-toggle="tab">Map</a></li>
-				<li><a href="#tab_media" data-toggle="tab">Media</a></li>
+				<li class="active"><a href="#tab_feed" data-toggle="tab">Read</a></li>
+				<li><a href="#tab_media" data-toggle="tab">View</a></li>
+				<li><a href="#tab_map" data-toggle="tab">Visit</a></li>
 			</ul>
 		</div>
 	</div>
@@ -23,20 +23,26 @@
 		</div>
 		<div class="row" id="tab_feed_content">
 			<div id="feed_container" class="span2">
-				<h3>Select Feed</h3>
 				<div id="tag_directory"></div>		
 			</div>
 			<div id="feeditem_container" class="span6">
-				<h3>Read Articles</h3>
 				<div id="feed_directory"></div>
 				<div class="pagination"></div>
 			</div>
 			<div id="reaction_container" class="span4 scroll-top">
-				<h3>Read Reactions</h3>
 				<div id="reaction_directory"></div>		
 			</div>
 		</div>
 	</div><!-- #tab_feed -->
+
+	<!-- #tab_media -->
+	<div class="tab-pane" id="tab_media">
+		<div class="row">
+			<div class="span12">
+				<div id="media_directory"></div>
+			</div>
+		</div>
+	</div><!-- #tab_media -->
 
 	<!-- #tab_map -->
 	<div class="tab-pane" id="tab_map">
@@ -45,15 +51,6 @@
 			</div>
 		</div>
 	</div><!-- #tab_map -->
-
-		<!-- #tab_media -->
-	<div class="tab-pane" id="tab_media">
-		<div class="row">
-			<div class="span12">
-				<p>howdy, media</p>
-			</div>
-		</div>
-	</div><!-- #tab_media -->
 
 </div>
 <?php $this->load->view('modal_permalink'); ?>
