37e0d7843385473773e13f56ffeb71b74b75d241
giuseppeserra <giuseppe.serra@gmail.com>
2012-10-23 18:09:08 +0200

    twitter login almost done

--- web/application/controllers/main.php
+++ web/application/controllers/main.php
@@ -18,11 +18,48 @@ class Main extends CI_Controller {
 		$this->data['logged_user'] = $logged_user;
 		$this->data['logged_admin'] = empty($logged_user['groups']) ? false : in_array( $admin_group, $logged_user['groups']);
 
+		
+		if ($this->session->userdata('detail_needed')){
+                    
+                    
+                    $this->data['template'] = 'user_detail';
+                    
+                    $this->load->library('form_validation');
+                    
+                    $this->data['first_name'] = array('name' => 'first_name',
+				'id' => 'first_name',
+				'type' => 'text',
+				'value' => $this->form_validation->set_value('first_name'),
+			);
+			$this->data['last_name'] = array('name' => 'last_name',
+				'id' => 'last_name',
+				'type' => 'text',
+				'value' => $this->form_validation->set_value('last_name'),
+			);
+			$this->data['email'] = array('name' => 'email',
+				'id' => 'email',
+				'type' => 'text',
+				'value' => $this->form_validation->set_value('email'),
+			);
+			
+			$this->data['password'] = array('name' => 'password',
+				'id' => 'password',
+				'type' => 'password',
+				'value' => $this->form_validation->set_value('password'),
+			);
+			$this->data['password_confirm'] = array('name' => 'password_confirm',
+				'id' => 'password_confirm',
+				'type' => 'password',
+				'value' => $this->form_validation->set_value('password_confirm'),
+			);
+
+                    
+                }else{
+                    $this->data['template'] = 'home';
+                }
+                
 		/**
 		 * controlla che l'utente abbia i dati personali completi, se sono completi allora carica home
-		 */
-		$this->data['template'] = 'home';
-		/**
 		 * altrimenti carica il template user_detail (dentro views/main)
 		 * $this->data['template'] = 'user_detail';
 		 */
@@ -30,6 +67,7 @@ class Main extends CI_Controller {
 
 	function index()
 	{
+                $this->session->set_userdata('detail_needed', false);
 		if ( !$this->logged_in ) {
 			redirect('/auth/login', 'refresh');
 		}
@@ -38,6 +76,81 @@ class Main extends CI_Controller {
 
 	function update_user() {
 		// fai tutte le tue belle cosine sul database e poi
+            
+             
+            //$this->data['title'] = "Create User";
+            
+            $this->load->library('form_validation');
+            
+            //validate form input
+            $this->form_validation->set_rules('first_name', 'First Name', 'required|xss_clean');
+            $this->form_validation->set_rules('last_name', 'Last Name', 'required|xss_clean');
+            $this->form_validation->set_rules('email', 'Email Address', 'required|valid_email');
+            $this->form_validation->set_rules('password', 'Password', 'required|min_length[' . $this->config->item('min_password_length', 'ion_auth') . ']|max_length[' . $this->config->item('max_password_length', 'ion_auth') . ']|matches[password_confirm]');
+            $this->form_validation->set_rules('password_confirm', 'Password Confirmation', 'required');
+
+		if ($this->form_validation->run() == true)
+		{
+			$username = strtolower($this->input->post('first_name')) . ' ' . strtolower($this->input->post('last_name'));
+			$email = $this->input->post('email');
+			$password = $this->input->post('password');
+
+			$additional_data = array('first_name' => $this->input->post('first_name'),
+				'last_name' => $this->input->post('last_name'),
+			);
+                        $id = $this->session->userdata('user_id');
+                        $data = array(
+					'email' => $email,
+					'password' => $password,
+					 );
+                        $this->ion_auth->update($id, $data);
+                        $this->logged_in = $this->ion_auth->login($email, $password);
+                        redirect('/', 'refresh');
+		}
+                
+                /*
+		if ($this->form_validation->run() == true && $this->ion_auth->register($username, $password, $email, $additional_data))
+		{ //check to see if we are creating the user
+			//redirect them back to the admin page
+                        $this->logged_in = $this->ion_auth->login($email, $password);
+			$this->session->set_flashdata('message', "User Created");
+			redirect('/', 'refresh');
+        	}*/
+		else
+		{ //display the create user form
+			//set the flash data error message if there is one
+			$this->data['message'] = (validation_errors() ? validation_errors() : ($this->ion_auth->errors() ? $this->ion_auth->errors() : $this->session->flashdata('message')));
+
+			$this->data['first_name'] = array('name' => 'first_name',
+				'id' => 'first_name',
+				'type' => 'text',
+				'value' => $this->form_validation->set_value('first_name'),
+			);
+			$this->data['last_name'] = array('name' => 'last_name',
+				'id' => 'last_name',
+				'type' => 'text',
+				'value' => $this->form_validation->set_value('last_name'),
+			);
+			$this->data['email'] = array('name' => 'email',
+				'id' => 'email',
+				'type' => 'text',
+				'value' => $this->form_validation->set_value('email'),
+			);
+			
+			$this->data['password'] = array('name' => 'password',
+				'id' => 'password',
+				'type' => 'password',
+				'value' => $this->form_validation->set_value('password'),
+			);
+			$this->data['password_confirm'] = array('name' => 'password_confirm',
+				'id' => 'password_confirm',
+				'type' => 'password',
+				'value' => $this->form_validation->set_value('password_confirm'),
+			);
+			$this->load->view('main/user_detail', $this->data);
+		}
+        
+        
 		redirect('/', 'refresh');
 	}
 
@@ -129,41 +242,66 @@ class Main extends CI_Controller {
 			$this->session->set_userdata('access_token', $access_token);
 			// Let's get the user's info 
 			$user_info = $this->twitteroauth->get('account/verify_credentials'); 
-			// Print user's info  
-			//print_r($user_info); 
-			$profile = null;
-			$profile['screen_name']=$user_info->screen_name;
-			//$profile['email']=$user_info->screen_name . '@twitter.com';
-			//$profile['id'] = $user_info->id;            
-                        $twitter_token = array(
+			
+                        $screen_name=$user_info->screen_name;
+			
+                        $profile = null;
+			$profile['name']=$user_info->screen_name;
+			$profile['email']=$user_info->screen_name . '@twitter.com';
+			$profile['id'] = $user_info->id;            
+                        $additional_info = array(
+                                'screen_name' => $screen_name,
 				'oauth_token' => $oauth_token,
 				'oauth_token_secret' => $oauth_token_secret);
                         
-                   
-			
+                   	
                         $this->tables  = $this->config->item('tables', 'ion_auth');
                         $query = $this->db->select('username, email, id, password, active, last_login')
-		                  ->where(sprintf("(screen_name = '%1\$s')", $this->db->escape_str($profile['screen_name'])))
+		                  ->where(sprintf("(screen_name = '%1\$s')", $this->db->escape_str($screen_name)))
 		                  ->limit(1)
 		                  ->get($this->tables['users']);
 
                         $user = $query->row();
                         
-                        // my problem
-                        //$login = $this->ion_auth->login($user->username, 'password');
+                        
                 	
-                        if (sizeof($user)==0) {
-                            redirect('/auth/create_user_twitter', 'refresh');
-                            //$this->ion_auth->create_user_twitter();
+                        if ($query->num_rows() == 1) {
+                            $session_data = array(
+                                'username'             => $user->username,
+                                'email'                => $user->email,
+                                'user_id'              => $user->id, //everyone likes to overwrite id so we'll use user_id
+                                'old_last_login'       => $user->last_login
+                            );
+
+                            $this->session->set_userdata($session_data);
+                            
+                        }else{
+                        
+                            
+                            
+                            $this->session->set_userdata('detail_needed', true);
+                            /*
+                            $session_data = array(
+                                'username'             => $screen_name,
+                                 'user_id'             => $user_info->id
+                                 );
+                            $this->session->set_userdata($session_data);
+                            */
+                            
+                            $this->ion_auth->register($profile['name'], $profile['id'], $profile['email'],$additional_info);
+                            $this->logged_in = $this->ion_auth->login($profile['email'], $profile['id']);
                         }
-                        
-                        
+                            
+                        /*
+                        $login=false;
                         
 			if( !$login ) {
-				$this->ion_auth->register($profile['name'], $profile['id'], $profile['email'],$twitter_token);
+				$this->ion_auth->register($profile['name'], $profile['id'], $profile['email'],$additional_info);
 				$this->logged_in = $this->ion_auth->login($profile['email'], $profile['id']);
 			}
-
+                        */
+                        
+                        /* code to extract the timeline and the entities
                         $home_timeline = $this->twitteroauth->get('statuses/home_timeline',array('count' => 200)); 
 			//print_r($home_timeline);
                         $timeline_content = null;
@@ -173,6 +311,8 @@ class Main extends CI_Controller {
                         
                         $this->load->model('scraper_model');
                         $entities=$this->scraper_model->scrape_stamat_ner( $timeline_content);
+                        
+                         */
                         redirect('/', 'refresh');
 
 		} else {  
--- web/application/views/main/user_detail.php
+++ web/application/views/main/user_detail.php
@@ -1,7 +1,34 @@
 <div class="row">
 	<div class="span12" id="auth_traditional">
-		<h1>beppeeeeeeeeeeeeeee</h1>
-		<p>insert your user form here</p>
-		<p>il form submit deve andare sul main controller nella funzione update_user</p>
-	</div>
+		    
+                <?php echo form_open("main/update_user");?>
+                <p>First Name:<br />
+                <?php echo form_input($first_name);?>
+                </p>
+      
+                <p>Last Name:<br />
+                <?php echo form_input($last_name);?>
+                </p>
+      
+                <p>Email:<br />
+                <?php echo form_input($email);?>
+                </p>
+        
+                <p>Password:<br />
+                <?php echo form_input($password);?>
+                </p>
+      
+                <p>Confirm Password:<br />
+                <?php echo form_input($password_confirm);?>
+                </p>
+      
+      
+                <p><?php echo form_submit('submit', 'Create User');?></p>
+
+      
+                <?php echo form_close();?>
+        
+        </div>
+     
+    
 </div>
